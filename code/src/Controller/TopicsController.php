d3c37e90f8d258d86c6b11ca6040daf083d51051
Vinícius Alves Hax <viniciushax@gmail.com>
2022-05-20 14:34:29 -0300

    Adicionando métodos: - view para visualizar um tópico - add para criar um tópico

--- code/src/Controller/TopicsController.php
+++ code/src/Controller/TopicsController.php
@@ -8,10 +8,45 @@ namespace App\Controller;
 
 class TopicsController extends AppController {
 
-	public function index() {
+	public function initialize(): void
+	{
+		parent::initialize();
+
 		$this->loadComponent('Paginator');
+		$this->loadComponent('Flash'); // Include the FlashComponent
+	}
+
+
+
+	public function index() {
 		$topics = $this->Paginator->paginate($this->Topics->find());
 		$this->set(compact('topics'));
 	}
+	
+	
+	public function view($slug)
+	{
+		$topic = $this->Topics->findBySlug($slug)->firstOrFail();
+		$this->set(compact('topic'));
+	}
+	
+	public function add()
+	{
+		$topic = $this->Topics->newEmptyEntity();
+		if ($this->request->is('post')) {
+		    $topic = $this->Topics->patchEntity($topic, $this->request->getData());
+
+		    // Hardcoding the user_id is temporary, and will be removed later
+		    // when we build authentication out.
+		    $topic->user_id = 1;
+
+		    if ($this->Topics->save($topic)) {
+			$this->Flash->success(__('Seu tópico foi criado.'));
+			return $this->redirect(['action' => 'index']);
+		    }
+		    $this->Flash->error(__('Não foi possível criar seu tópico.'));
+		}
+		$this->set('topic', $topic);
+	}	
 
 }
